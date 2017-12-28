f71a956bfb84239b6d809bdd7ea5b4f79182dd09
czebiniak <30630518+czebiniak@users.noreply.github.com>
2017-12-28 16:22:23 -0500

    Ver 1.0
    Script is born!

--- transfer_backup.sh
+++ transfer_backup.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 # VARS
 SERVERPASS='##'
-RSYNCORIGINSERVER='kithcart.us'
-RSYNCORIGINUSER='root'
-DESTINATIONTOBECOPIED=/Ark/ShooterGame/Saved
-COPYTO='/Ark'
+RSYNCORIGINSERVER='##'
+RSYNCORIGINUSER='##'
+DESTINATIONTOBECOPIED='/Ark/ShooterGame/Saved'
+COPYTO='/Ark/ShooterGame/Saved'
 # PREREQS
 sudo apt-get install sshpass rsync -y
 # EXECUTION
+if [ ! -d /Ark ]
+	then
+	mkdir /Ark
+	chmod -R 777 /Ark
+fi
 sshpass -p '$SERVERPASS' rsync --rtvuc --progress $RSYNCORIGINUSER@$RSYNCORIGINSERVER:$DESTINATIONTOBECOPIED $COPYTO
 echo "Finished"
 exit 0
