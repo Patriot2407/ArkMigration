5f1d9efc6330643a4473f49186ec0522b04a1244
czebiniak <30630518+czebiniak@users.noreply.github.com>
2017-12-28 16:55:24 -0500

    Ver1.0
    IMPORTANT

--- transfer_backup.sh
+++ transfer_backup.sh
@@ -6,13 +6,15 @@ RSYNCORIGINUSER='##'
 DESTINATIONTOBECOPIED='/Ark/ShooterGame/Saved'
 COPYTO='/Ark/ShooterGame/Saved'
 # PREREQS
-sudo apt-get install sshpass rsync -y
+sudo apt-get install rsync -y
 # EXECUTION
 if [ ! -d /Ark ]
 	then
 	mkdir /Ark
 	chmod -R 777 /Ark
 fi
-sshpass -p '$SERVERPASS' rsync --rtvuc --progress $RSYNCORIGINUSER@$RSYNCORIGINSERVER:$DESTINATIONTOBECOPIED $COPYTO
+clear
+echo "Starting rsync. Please wait..."
+ssh $RSYNCORIGINUSER@$RSYNCORIGINSERVER rsync -rtvuc --progress $DESTINATIONTOBECOPIED $COPYTO >> /Scripts/rsync.log
 echo "Finished"
 exit 0
